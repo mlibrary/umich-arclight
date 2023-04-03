384c45a65cf946e3254bce6539ba0155c57edd09
Greg Kostin <gkostin@umich.edu>
2023-04-03 10:31:04 -0400

    index finding aid job refactor

--- spec/jobs/index_finding_aid_job_spec.rb
+++ spec/jobs/index_finding_aid_job_spec.rb
@@ -4,23 +4,40 @@ require 'rails_helper'
 RSpec.describe IndexFindingAidJob, type: :job do
   include ActiveJob::TestHelper
 
-  let(:file) { 'path/to/file.xml' }
-  let(:fd) { StringIO.open("<ead><eadheader><eadid>eadid.slug</eadid></eadheader></ead>") }
-  let(:repository_id) { 'repo' }
-  let(:path) { "#{DulArclight.finding_aid_data}/xml/#{repository_id}" }
-  let(:dest) { "#{path}/eadid-slug.xml" }
-  let(:stdout_and_stderr) { 'stdout and stderr' }
-  let(:process_status) { instance_double("Process::Status", "process_status", success?: success) }
-  let(:success) { true }
+  let(:src_path) { 'path/to/file.xml' }
+  let(:fd) { StringIO.open(xml) }
+  let(:xml) do
+    <<~EOS
+      <ead>
+        <eadheader>
+          #{eadid_tag}
+        </eadheader>
+          <archdesc level="1">
+            <did>
+              <unittitle>title</unittitle>
+            </did>
+          </archdesc>
+      </ead>
+    EOS
+  end
+  let(:eadid_tag) { "<eadid>#{eadid}</eadid>" }
+  let(:eadid) { "eadid.slug" }
+  let(:eadid_slug) { "eadid-slug" }
+  let(:repo_id) { 'repo' }
+  let(:repository) { class_double("Arclight::Repository", "repository", name: "repository") }
+  let(:dest_dir) { "#{DulArclight.finding_aid_data}/xml/#{repo_id}" }
+  let(:dest_path) { "#{dest_dir}/#{eadid_slug}.xml" }
+  let(:err_msg) { "Error Message" }
 
   before do
-    allow(Open3).to receive(:capture2e).with({"REPOSITORY_ID" => repository_id},
-                                             "bundle exec traject -u #{Blacklight.default_index.connection.base_uri.to_s.chomp("/")} -i xml -c ./lib/dul_arclight/traject/ead2_config.rb #{file}")
-      .and_return([stdout_and_stderr, process_status])
     allow(File).to receive(:open).and_call_original
-    allow(File).to receive(:open).with(file, "r:UTF-8:UTF-8").and_yield(fd)
-    allow(FileUtils).to receive(:mkdir_p).with(path)
-    allow(FileUtils).to receive(:copy_file).with(file, dest, {dereference: true, preserve: true, remove_destination: true})
+    allow(File).to receive(:open).with(src_path, "r:UTF-8:UTF-8").and_yield(fd)
+    allow(Arclight::Repository).to receive(:find_by).with(slug: repo_id).and_return(repository)
+    allow(FileUtils).to receive(:mkdir_p).with(dest_dir)
+    allow(FileUtils).to receive(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
+    allow(IngestAutomationJob).to receive(:perform_later).and_call_original
+    allow(IngestAutomationJob).to receive(:perform_later).with('index.success', src_path: src_path, archive_path: dest_path, ead_id: eadid_slug)
+    allow(IngestAutomationJob).to receive(:perform_later).with('index.failure', src_path: src_path, archive_path: dest_path, ead_id: eadid_slug, err_msg: err_msg)
   end
 
   after do
@@ -29,52 +46,103 @@ RSpec.describe IndexFindingAidJob, type: :job do
   end
 
   it 'queues the job' do
-    expect { described_class.perform_later(file, repository_id) }.to have_enqueued_job(described_class).with(file, repository_id).on_queue("index")
+    expect { described_class.perform_later(src_path, repo_id) }.to have_enqueued_job(described_class).with(src_path, repo_id).on_queue("index")
   end
 
-  it 'puts stdout and stderr' do
-    expect { perform_enqueued_jobs { described_class.perform_later(file, repository_id) } }.to output(stdout_and_stderr + "\n").to_stdout_from_any_process
+  it 'performs the job' do
+    expect { perform_enqueued_jobs { described_class.perform_later(src_path, repo_id) } }.not_to raise_exception
+    expect(IngestAutomationJob).to have_received(:perform_later).with('index.success', src_path: src_path, archive_path: dest_path, ead_id: eadid_slug)
   end
 
-  it 'copies the source file to data xml repo directory using eadid slug' do
-    perform_enqueued_jobs { described_class.perform_later(file, repository_id) }
-    expect(FileUtils).to have_received(:mkdir_p).with(path)
-    expect(FileUtils).to have_received(:copy_file).with(file, dest, {dereference: true, preserve: true, remove_destination: true})
+  it 'copies the source file to data xml repo directory using ead id' do
+    perform_enqueued_jobs { described_class.perform_later(src_path, repo_id) }
+    expect(FileUtils).to have_received(:mkdir_p).with(dest_dir)
+    expect(FileUtils).to have_received(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
   end
 
-  context 'when empty eadid tag' do
-    let(:fd) { StringIO.open("<ead><eadheader><eadid></eadid></eadheader></ead>") }
-    let(:dest) { "#{path}/#{File.basename(file, ".*")}.xml" }
+  context 'when eadid tag' do
+    let(:err_msg) { /Document is missing mandatory uniqueKey field: id/ }
 
-    it 'copies the source file to data xml repo directory using basename' do
-      perform_enqueued_jobs { described_class.perform_later(file, repository_id) }
-      expect(FileUtils).to have_received(:mkdir_p).with(path)
-      expect(FileUtils).to have_received(:copy_file).with(file, dest, {dereference: true, preserve: true, remove_destination: true})
+    context 'when value empty' do
+      let(:eadid) { "" }
+
+      it 'raises an exception' do
+        expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+        expect(IngestAutomationJob).to have_received(:perform_later).with('index.failure', src_path: src_path, archive_path: nil, ead_id: nil, err_msg: err_msg)
+      end
+
+      it 'does NOT copy source file to data xml repo directory' do
+        expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+        expect(FileUtils).not_to have_received(:mkdir_p).with(dest_path)
+        expect(FileUtils).not_to have_received(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
+      end
+    end
+
+    context 'when tag missing' do
+      let(:eadid_tag) { "" }
+
+      it 'raises an exception' do
+        expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+        expect(IngestAutomationJob).to have_received(:perform_later).with('index.failure', src_path: src_path, archive_path: nil, ead_id: nil, err_msg: err_msg)
+      end
+
+      it 'does NOT copy source file to data xml repo directory' do
+        expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+        expect(FileUtils).not_to have_received(:mkdir_p).with(dest_path)
+        expect(FileUtils).not_to have_received(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
+      end
     end
   end
 
-  context 'when missing eadid tag' do
-    let(:fd) { StringIO.open("<ead><eadheader></eadheader></ead>") }
-    let(:dest) { "#{path}/#{File.basename(file, ".*")}.xml" }
-
-    it 'copies the source file to data xml repo directory using basename' do
-      perform_enqueued_jobs { described_class.perform_later(file, repository_id) }
-      expect(FileUtils).to have_received(:mkdir_p).with(path)
-      expect(FileUtils).to have_received(:copy_file).with(file, dest, {dereference: true, preserve: true, remove_destination: true})
+  context 'when missing file' do
+    before do
+      allow(File).to receive(:open).with(src_path, "r:UTF-8:UTF-8").and_raise(StandardError, err_msg)
     end
-  end
-
-  context 'when traject failure' do
-    let(:success) { false }
 
     it 'raises an exception' do
-      expect { described_class.perform_now(file, repository_id) }.to raise_exception(DulArclight::IndexError, stdout_and_stderr)
+      expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+      expect(IngestAutomationJob).to have_received(:perform_later).with('index.failure', src_path: src_path, archive_path: nil, ead_id: nil, err_msg: err_msg)
     end
 
     it 'does NOT copy source file to data xml repo directory' do
-      expect { described_class.perform_now(file, repository_id) }.to raise_exception(DulArclight::IndexError, stdout_and_stderr)
-      expect(FileUtils).not_to have_received(:mkdir_p).with(path)
-      expect(FileUtils).not_to have_received(:copy_file).with(file, dest, {dereference: true, preserve: true, remove_destination: true})
+      expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+      expect(FileUtils).not_to have_received(:mkdir_p).with(dest_path)
+      expect(FileUtils).not_to have_received(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
+    end
+  end
+
+  context 'when repo nil' do
+    let(:repo_id) { nil }
+    let(:err_msg) { /no implicit conversion of nil into String/ }
+
+    it 'raises an exception' do
+      expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+      expect(IngestAutomationJob).to have_received(:perform_later).with('index.failure', src_path: src_path, archive_path: nil, ead_id: eadid_slug, err_msg: err_msg)
+    end
+
+    it 'does NOT copy source file to data xml repo directory' do
+      expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+      expect(FileUtils).not_to have_received(:mkdir_p).with(dest_path)
+      expect(FileUtils).not_to have_received(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
+    end
+  end
+
+  context 'when missing repo' do
+    let(:err_msg) { /undefined method `name' for nil:NilClass/ }
+
+    before do
+      allow(Arclight::Repository).to receive(:find_by).with(slug: repo_id).and_return(nil)
+    end
+
+    it 'raises an exception' do
+      expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+      expect(IngestAutomationJob).to have_received(:perform_later).with('index.failure', src_path: src_path, archive_path: nil, ead_id: nil, err_msg: err_msg)
+    end
+
+    it 'does NOT copy source file to data xml repo directory' do
+      expect { described_class.perform_now(src_path, repo_id) }.to raise_exception(DulArclight::IndexError, err_msg)
+      expect(FileUtils).not_to have_received(:mkdir_p).with(dest_path)
+      expect(FileUtils).not_to have_received(:copy_file).with(src_path, dest_path, {dereference: true, preserve: true, remove_destination: true})
     end
   end
 end
