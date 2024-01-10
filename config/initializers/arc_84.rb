9914be7b2838c25bd2ec533a1b739f3596c5dde6
Chris Powell <sooty@umich.edu>
2024-01-10 16:21:04 -0500

    Update arc_84.rb
    Added redirect for hungarywar to ams0195

--- config/initializers/arc_84.rb
+++ config/initializers/arc_84.rb
@@ -1114,4 +1114,5 @@ ARC_84_EADID_MAP = {
   "umich-wcl-P-2863-1smi" => "umich-wcl-P-2863.1smi",
   "umich-wcl-P-2863-25for" => "umich-wcl-P-2863.25for",
   "umich-scl-kramera" => "umich-scl-ams0185"
+  "umich-scl-hungarywar" => "umich-scl-ams0195"
 }
